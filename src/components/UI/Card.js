fa8feb925ae123060a13b3d103e60bd56977d71b
KiHoon Roh <started677@gmail.com>
2022-05-09 20:43:51 +0900

    global CSS 추가

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,8 +1,14 @@
 import styled from "@emotion/styled";
 
 export const Card = styled.div`
+    display: flex;
     padding: 1rem;
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.25);
     border-radius: 14px;
     background-color: white;
+    transition: all 0.25s ease-in-out;
+
+    &:hover {
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.25);
+    }
 `;
--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,27 +1,20 @@
 import React from "react";
 import Navigation from "@components/Navigation";
 import Footer from "@components/Footer";
-import styled from "@emotion/styled";
 
 const Layout = (props) => {
     return props.auth ? (
         <>
-            <Main>{props.children}</Main>
+            <>{props.children}</>
             <Footer />
         </>
     ) : (
         <>
             <Navigation />
-            <Main>{props.children}</Main>
+            <>{props.children}</>
             <Footer />
         </>
     );
 };
 
 export default Layout;
-
-const Main = styled.main`
-    width: 90vw;
-    margin: 0 auto;
-    padding: 3rem 2rem;
-`;
