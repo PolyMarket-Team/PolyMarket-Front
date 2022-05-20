2da1124fa18293746ff837287c867dbabae37c1c
KiHoon Roh <started677@gmail.com>
2022-05-20 20:34:01 +0900

    .env 환경 변수 설정

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const api = axios.create({ baseURL: "https://api-dev.polymarket.kr/" });
 
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL });
 export const sendEmail = (data) => api.post("users/send-email", data);
 export const confirmEmail = (data) => api.post("users/confirm-email", data);
 export const signUp = (formData) => api.post("users/signup", formData);
