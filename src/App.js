7ab979f06c81a70dacdb216dae22edb1240d0410
KiHoon Roh <started677@gmail.com>
2022-06-13 18:19:02 +0900

    로그인 유지 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import loadable from "@loadable/component";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginCheck } from "redux/modules/authSlice";
 
 const Home = loadable(() => import("@pages/Home"));
 const Product = loadable(() => import("@pages/Product"));
@@ -9,19 +12,27 @@ const Profile = loadable(() => import("@pages/Profile"));
 const Login = loadable(() => import("@pages/Login"));
 const Signup = loadable(() => import("@pages/Signup"));
 
-function App() {
+const App = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(loginCheck());
+    }, []);
+
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/product" element={<Product />} />
-            <Route path="/product/:productId" element={<ProductDetail />} />
-            <Route path="/chat" element={<Chat />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/product" element={<Product />} />
+                <Route path="/product/:productId" element={<ProductDetail />} />
+                <Route path="/chat" element={<Chat />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </>
     );
-}
+};
 
 export default App;
--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -6,8 +6,12 @@ import { Link, NavLink } from "react-router-dom";
 import Logo from "@components/UI/Logo";
 import { Button } from "@components/UI/Button";
 import { Navbar, NavbarWrap, MyButton, MyMenu, MyMenuItem } from "./style";
+import { logOut } from "redux/modules/authSlice";
+import { toast } from "react-toastify";
 
 const Navigation = () => {
+    const { isLogin, userInfo } = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
     const [anchorEl, setAnchorEl] = useState(null);
     const open = Boolean(anchorEl);
 
@@ -24,7 +28,8 @@ const Navigation = () => {
             <NavbarWrap>
                 <Logo logo="color" />
 
-                {/* <div>
+                {isLogin && userInfo !== "" ? (
+                    <div>
                         <MyButton
                             id="basic-button"
                             aria-controls={open ? "basic-menu" : undefined}
@@ -32,7 +37,7 @@ const Navigation = () => {
                             aria-expanded={open ? "true" : undefined}
                             onClick={handleClick}
                         >
-                            {`${nickname}`}
+                            {`${userInfo.nickname}`}
                         </MyButton>
                         <MyMenu
                             sx={{ width: "auto" }}
@@ -47,13 +52,21 @@ const Navigation = () => {
                             <Link to="/profile">
                                 <MyMenuItem>프로필</MyMenuItem>
                             </Link>
-                            <MyMenuItem onClick={logOut}>로그아웃</MyMenuItem>
+                            <MyMenuItem
+                                onClick={() => {
+                                    dispatch(logOut());
+                                    toast.error("로그아웃 되었습니다.");
+                                }}
+                            >
+                                로그아웃
+                            </MyMenuItem>
                         </MyMenu>
-                    </div> */}
-
-                <NavLink to="/login">
-                    <Button>로그인</Button>
-                </NavLink>
+                    </div>
+                ) : (
+                    <NavLink to="/login">
+                        <Button>로그인</Button>
+                    </NavLink>
+                )}
             </NavbarWrap>
         </Navbar>
     );
